7b7da7969325aac143de41c317fcd52607d73c4f
gl <lordz@prorobotech.ru>
2025-05-30 15:33:28 +0300

    delete webhook service

--- internal/webhook/v1alpha1/service_webhook.go
+++ internal/webhook/v1alpha1/service_webhook.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -50,7 +51,7 @@ func SetupServiceWebhookWithManager(mgr ctrl.Manager) error {
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-// +kubebuilder:webhook:path=/validate-netguard-sgroups-io-v1alpha1-service,mutating=false,failurePolicy=fail,sideEffects=None,groups=netguard.sgroups.io,resources=services,verbs=create;update,versions=v1alpha1,name=vservice-v1alpha1.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-netguard-sgroups-io-v1alpha1-service,mutating=false,failurePolicy=fail,sideEffects=None,groups=netguard.sgroups.io,resources=services,verbs=create;update;delete,versions=v1alpha1,name=vservice-v1alpha1.kb.io,admissionReviewVersions=v1
 
 // ServiceCustomValidator struct is responsible for validating the Service resource
 // when it is created, updated, or deleted.
@@ -165,7 +166,76 @@ func (v *ServiceCustomValidator) ValidateDelete(ctx context.Context, obj runtime
 	}
 	servicelog.Info("Validation for Service upon deletion", "name", service.GetName())
 
-	// TODO(user): fill in your validation logic upon object deletion.
+	// 1. Get all ServiceAlias resources in the same namespace
+	serviceAliasList := &netguardv1alpha1.ServiceAliasList{}
+	if err := v.Client.List(ctx, serviceAliasList, client.InNamespace(service.GetNamespace())); err != nil {
+		servicelog.Error(err, "Failed to list ServiceAlias objects")
+		return nil, fmt.Errorf("failed to list ServiceAlias objects: %w", err)
+	}
+
+	// 2. Filter ServiceAlias resources that have this Service as owner
+	var ownedAliases []netguardv1alpha1.ServiceAlias
+	for _, alias := range serviceAliasList.Items {
+		for _, ownerRef := range alias.GetOwnerReferences() {
+			if ownerRef.UID == service.GetUID() {
+				ownedAliases = append(ownedAliases, alias)
+				break
+			}
+		}
+	}
+
+	// If no aliases are owned by this service, allow deletion
+	if len(ownedAliases) == 0 {
+		return nil, nil
+	}
+
+	// 3. Check if any of the owned aliases are used in active RuleS2S resources
+	ruleS2SList := &netguardv1alpha1.RuleS2SList{}
+	if err := v.Client.List(ctx, ruleS2SList); err != nil {
+		servicelog.Error(err, "Failed to list RuleS2S objects")
+		return nil, fmt.Errorf("failed to list RuleS2S objects: %w", err)
+	}
+
+	// Map to store aliases referenced by rules
+	aliasesWithRules := make(map[string][]string)
+
+	// Check each rule for references to any of our owned aliases
+	for _, rule := range ruleS2SList.Items {
+		for _, alias := range ownedAliases {
+			// Check if the rule references this alias as local service
+			if rule.Spec.ServiceLocalRef.Name == alias.Name &&
+				rule.Namespace == alias.Namespace {
+				aliasesWithRules[alias.Name] = append(aliasesWithRules[alias.Name],
+					fmt.Sprintf("RuleS2S '%s' (as local service)", rule.Name))
+			}
+
+			// Check if the rule references this alias as target service
+			targetNamespace := rule.Spec.ServiceRef.ResolveNamespace(rule.Namespace)
+			if rule.Spec.ServiceRef.Name == alias.Name &&
+				targetNamespace == alias.Namespace {
+				aliasesWithRules[alias.Name] = append(aliasesWithRules[alias.Name],
+					fmt.Sprintf("RuleS2S '%s' (as target service)", rule.Name))
+			}
+		}
+	}
+
+	// If there are any aliases with rules, prevent deletion
+	if len(aliasesWithRules) > 0 {
+		var errorMsgs []string
+		errorMsgs = append(errorMsgs, fmt.Sprintf("Cannot delete Service '%s' because it has ServiceAliases that are referenced by active RuleS2S resources:", service.Name))
+
+		for aliasName, rules := range aliasesWithRules {
+			errorMsgs = append(errorMsgs, fmt.Sprintf("  ServiceAlias '%s' is referenced by:", aliasName))
+			for _, rule := range rules {
+				errorMsgs = append(errorMsgs, fmt.Sprintf("    - %s", rule))
+			}
+		}
+
+		errorMsg := strings.Join(errorMsgs, "\n")
+		servicelog.Info("Preventing Service deletion due to active RuleS2S references",
+			"service", service.Name, "errorMsg", errorMsg)
+		return nil, fmt.Errorf(errorMsg)
+	}
 
 	return nil, nil
 }
